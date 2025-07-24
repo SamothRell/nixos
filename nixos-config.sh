8a4ff73b7dee798e1f773a1a19653d2b089a15ec
Thomas Muller <97891227+SamothRell@users.noreply.github.com>
2025-07-24 11:51:20 +0200

    Update nixos-config.sh

--- nixos-config.sh
+++ nixos-config.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+wget https://github.com/samothrell/nixos/archive/main.tar.gz
+tar -xzvf main.tar.gz
+rm main.tar.gz
+mv nixos-main/ nixos
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+sudo nix-channel --update
+
+cat >> /etc/nixos/configuration.nix << EOL
+home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.thomas = import ./home.nix;
+  };
+EOL
+sudo cp /etc/nixos/*.nix ~/nixos
+sudo chown -R 1000:100 ~/nixos
+sudo rm -r /etc/nixos
+sudo ln -s ~/nixos /etc/nixos
